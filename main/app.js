810b76d937027c59c13e3851c48f9f9ce3c64fe6
CristinaTudor522 <tudorcristina18@stud.ase.ro>
2020-11-26 11:39:09 +0200

    Rezolvat

--- main/app.js
+++ main/app.js
@@ -1,5 +1,41 @@
 function addTokens(input, tokens){
+    //daca input  nu este de tip string 
+    if (typeof input !== 'string')
+{
+    //afisez un mesaj sugestiv
+    throw Error('Invalid input')
     
+}
+// daca input nu are lungimea corespunzatoare
+else if(input.length<6)
+{
+    //afisez un mesaj sugestiv
+    throw Error('Input should have at least 6 characters')
+}
+//pt vector 
+for(var i =0;i<tokens.length;i++)
+{
+    // daca  nu se verifica formatul 
+    if(typeof((tokens[i].tokenName))!=='string')
+    //afisez un mesaj sugestiv
+    throw Error('Invalid array format')
+}
+
+//in cazul in care input nu contine ...
+ if(input.includes('...')!=true){
+return input;}
+
+    //daca input contine ...
+    for(let i=0;i<tokens.length;i++){
+        let a=tokens[i]["tokenName"]
+        let b="${"+a+"}"
+        let c="..."
+        let position=input.indexOf('...')
+        if(position!==-1){
+            input=input.replace(c,b)
+        }
+    }
+    return input
 }
 
 const app = {
